9d21f4214f21643937adaf77e3b62277f4849e7c
Matt <mw283@cam.ac.uk>
2019-07-22 17:18:03 +0100

    new file:   timeout-occurred/patch.cpp 	modified:   timeout-occurred/patch.xodp

---
+++ timeout-occurred/patch.cpp
@@ -0,0 +1,18 @@
+
+struct State {
+};
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    // The node responds only if there is an input pulse
+    if (!isInputDirty<input_UPD>(ctx))
+        return;
+
+    // Get a pointer to the `VL53L0X` class instance
+    auto sensor = getValue<input_DEV>(ctx);
+
+    emitValue<output_TO>(ctx,sensor->timeoutOccurred());
+    emitValue<output_DONE>(ctx,1);
+
+}
